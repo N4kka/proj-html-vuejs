322e7183dc9447f84355052f0e00e413222ce6a1
N4kka <sarlinicola7@gmail.com>
2022-05-30 10:56:27 +0200

    Scaffolding

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <h1>prova</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<style scoped lang="scss">
+header {
+  height: 120px;
+  background-color: white;
+}
+</style>
